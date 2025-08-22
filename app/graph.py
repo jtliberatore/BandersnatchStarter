7a27681c53108b41e81ff7e0a7a399632978dcce
Josh <myschoolJTL@yahoo.com>
2025-08-22 18:55:47 -0400

    grafupload

--- app/graph.py
+++ app/graph.py
@@ -1,29 +1,15 @@
-from altair import Chart
+from altair import Chart, Tooltip
+from pandas import DataFrame
 
 
-def chart(df, x, y, target) -> Chart:
-    
-
-    graph = (
-        Chart(
-            df,
-            title=f"{y} by {x} for {target}",
-        )
-        .mark_circle(size=100)
-        .encode(
-            x=x,
-            y=y,
-            color=target,
-            tooltip=list(df.columns),
-        )
-        .configure_axis(
-            labelFontSize=12,
-            titleFontSize=14,
-        )
-        .configure_view(
-            strokeWidth=0,
-        )
+def chart(df: DataFrame, x, y, target) -> Chart:
+    graph = Chart(
+        df, 
+        title="Level vs Health by Type"
+    ).mark_circle(size=100).encode(
+        x="Level:Q",
+        y="Health:Q",
+        color="Type:N",
+        tooltip=Tooltip(["Name", "Type", "Level", "Health"])
     )
-
     return graph
-
